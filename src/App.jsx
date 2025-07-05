7478586a4edf172403c83c26a090fc4b614cc19e
LuvTrippin <sergeevtimursh@gmail.com>
2025-07-05 11:32:03 +0500

    ref: убрал basename из Router

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
     return (
-        <Router basename="/kinopedia">
+        <Router>
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="movie/:id" element={<MoviePage />} />
